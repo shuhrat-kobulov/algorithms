d6d8ff3d4433e058850f9452a5f4e782b7bd4f63
mrshukhratbek <mrshukhratbek>
2022-09-24 22:47:48 +0500

    Solve binary search

---
+++ 1.leetcode_problems_easy/704.binary_search/index.js
@@ -0,0 +1,18 @@
+const search = function (arr, item) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] === item) {
+      return mid;
+    } else if (arr[mid] < item) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+};
