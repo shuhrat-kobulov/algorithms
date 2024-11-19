c29a0f7479a7306ef262b1bbcda562a18e8d136d
shuhrat-kobulov <shukhratbekqobulov@gmail.com>
2024-11-19 15:42:13 +0500

    724. Find Pivot Index solution

---
+++ leetcode_problems_easy/724.find_pivot_index/index.ts
@@ -0,0 +1,17 @@
+function pivotIndex(nums: number[]): number {
+  const totalSum: number = nums.reduce((acc, num) => acc + num, 0);
+
+  let leftSum: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let rightSum = totalSum - leftSum - nums[i];
+
+    if (rightSum === leftSum) {
+      return i;
+    }
+
+    leftSum += nums[i];
+  }
+
+  return -1;
+}
