043acecf1527d86774805f56ddfeef2f23b1513a
shuhrat-kobulov <shukhratbekqobulov@gmail.com>
2024-11-20 15:35:34 +0500

    66. Plus One solution

--- leetcode_problems_easy/14.longest_common_prefix/index.ts
+++ leetcode_problems_easy/14.longest_common_prefix/index.ts
@@ -1,18 +1,18 @@
-function longestCommonPrefix (strs: string[]): string {
+function longestCommonPrefix(strs: string[]): string {
   if (!strs.length || strs.length >= 200) return '';
 
   let prefix: string = '';
 
   for (let i = 0; i < strs[0].length; i++) {
-    let charecter: string = strs[0][i];
+    let character: string = strs[0][i];
     for (let j = 0; j < strs.length; j++) {
-      if (strs[j][i] !== charecter) return prefix;
+      if (strs[j][i] !== character) return prefix;
     }
 
-    prefix += charecter;
+    prefix += character;
   }
 
   return prefix;
-};
+}
 
-longestCommonPrefix(['flower', 'flow', 'flight'])
+longestCommonPrefix(['flower', 'flow', 'flight']);
---
+++ leetcode_problems_easy/66.plus_one/index.ts
@@ -0,0 +1,14 @@
+function plusOne(digits: number[]): number[] {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] = digits[i] + 1;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+
+  return digits;
+}
